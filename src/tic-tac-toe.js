2fe6fccec6d1da07d63036710b0eced99dbe1758
spiderVS <fuzzy003@gmail.com>
2021-08-29 03:34:31 +0300

    feat: add solve tic-tac-toe task

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,34 +1,52 @@
 class TicTacToe {
     constructor() {
-
+      this.currentPlayer = 'x';
+      this.gameField = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+      ];
     }
 
     getCurrentPlayerSymbol() {
-
+      return this.currentPlayer;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+      if ((this.gameField[rowIndex][columnIndex] === null))  {
+        this.gameField[rowIndex][columnIndex] = this.currentPlayer;
+        this.currentPlayer = this.currentPlayer === 'x' ? 'o' : 'x';
+      } else return;
     }
 
     isFinished() {
-
+     return ((this.getWinner() != null) || (this.isDraw())) ? true : false;
     }
 
     getWinner() {
+      for (let i = 0; i < this.gameField.length; i++) {
+        if ((this.gameField[i].every(el => el === 'x')) || (this.gameField[i].every(el => el === 'o'))) return this.gameField[i][1];
 
+        if ((this.gameField[0][i] === this.gameField[1][i]) && (this.gameField[1][i] === this.gameField[2][i]) && ((this.gameField[0][i] === 'x') || (this.gameField[1][i] === 'o'))) return this.gameField[1][i];
+      }
+
+      if ((this.gameField[0][0] === this.gameField[1][1]) && (this.gameField[1][1] === this.gameField[2][2]) && ((this.gameField[0][0] === 'x') || (this.gameField[0][0] === 'o'))) return this.gameField[0][0];
+
+      if ((this.gameField[2][0] === this.gameField[1][1]) && (this.gameField[1][1] === this.gameField[0][2]) && ((this.gameField[2][0] === 'x') || (this.gameField[2][0] === 'o'))) return this.gameField[2][0];
+
+      return null;
     }
 
     noMoreTurns() {
-
+      return this.gameField.flat().every(el => el != null);
     }
 
     isDraw() {
-
+      return ((this.getWinner() === null) && (this.noMoreTurns()));
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+      return this.gameField[rowIndex][colIndex];
     }
 }
 
